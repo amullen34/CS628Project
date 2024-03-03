a4ae20965b17e9c143123d1acf42d081ea90b1d5
Aaron Mullen <aaron.mullen34@gmail.com>
2024-03-03 14:50:18 -0500

    Implemented multivariate time series classificastion

--- timeseries.py
+++ timeseries.py
@@ -1,54 +1,54 @@
 import pandas as pd
 import csv
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
 import numpy as np
 from pyts.classification import TimeSeriesForest
+from pyts.multivariate.classification import MultivariateClassifier
 
 
 def slidingWindow(df, sampling_rate, measurement):
     window_size = 5 * sampling_rate #Window size 5 seconds
     step_size = sampling_rate #Step size 1 second
-    # num_windows = int((df['timestamp'].max() - df['timestamp'].min()).total_seconds() / (step_size/sampling_rate))
     num_windows = int(len(df) / step_size)
     indexer = np.arange(window_size)[None, :] + step_size * np.arange(num_windows)[:, None] #Create indices for sliding window
     num_na = int(window_size/step_size - 1) #Remove the last few rows that will have missing values
     indexer = indexer[:-num_na]
 
     values = df[measurement].values
-    df_new = pd.DataFrame(values[indexer], columns=[f'Reading_{i}' for i in range(window_size)]) #Convert to dataframe
-
-    df_new['class'] = df.iloc[0]['class']
-    return df_new
+    return values[indexer] #Apply indexer to values
 
 def transform_data(data):
     sampling_rate = 50
-    measurement = 'chest_acceleration_z'
-    df = data[[measurement, 'class']]
-    new_df = pd.DataFrame()
-    for classification in df['class'].unique():
-        class_subset = df[df['class'] == classification]
-        class_subset_window = slidingWindow(class_subset, sampling_rate, measurement)
-        new_df = pd.concat([new_df, class_subset_window])
-    new_df = (new_df.reset_index()).drop(['index'], axis=1)
-    return new_df
+    X_overall = None
+    y_overall = None
+    for i, classification in enumerate(data['class'].unique()): #Must separate each classification
+        class_subset = data[data['class'] == classification]
+        X = None
+        for j, measurement in enumerate(data.columns.unique()): #Loop for each feature
+            if measurement != 'class':
+                class_subset_window = slidingWindow(class_subset, sampling_rate, measurement)
+                if j == 0:
+                    X = class_subset_window
+                elif j == 1:
+                    X = np.stack((X, class_subset_window), axis=1)
+                else:
+                    X = np.concatenate((X, class_subset_window[:, np.newaxis, :]), axis=1)
+        y = np.full((X.shape[0],), classification) #Create y array of class label
+        if i == 0:
+            X_overall = X
+            y_overall = y
+        else:
+            X_overall = np.concatenate((X_overall, X), axis=0)
+            y_overall = np.concatenate((y_overall, y), axis=0)
+    return X_overall, y_overall #Return X and y numpy arrays
 
-def dotimeseriesclassification(train, test):
-
-    X_train = train.drop(['class'], axis=1).values
-    y_train = train['class'].values
-    X_test = test.drop(['class'], axis=1).values
-    y_test = test['class'].values
-    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+def dotimeseriesclassification(X_train, X_test, y_train, y_test):
     print(X_train.shape)
     print(X_test.shape)
     print(y_train.shape)
     print(y_test.shape)
-    clf = TimeSeriesForest(random_state=42, class_weight='balanced', n_jobs=-1)
+    clf = MultivariateClassifier(TimeSeriesForest(random_state=42, class_weight='balanced', n_jobs=-1))
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     print(classification_report(y_test, y_pred))
@@ -67,8 +67,10 @@ classes = {0: 'inbetween', 1: 'standing still', 2: 'sitting and relaxing', 3: 'l
            5: 'climbing stairs', 6: 'waist bends forward', 7: 'frontal elevation of arms',
            8: 'knees bending (crouching)', 9: 'cycling', 10: 'jogging', 11: 'running', 12: 'jump front and back'}
 
-training_data = pd.DataFrame()
-test_data = pd.DataFrame()
+X_train = None
+X_test = None
+y_train = None
+y_test = None
 for i in range(1,11):
     file_contents = []
     with open('MHEALTHDATASET/mHealth_subject'+str(i)+'.log', 'r') as file: #Read in log file
@@ -78,20 +80,20 @@ for i in range(1,11):
         data = pd.DataFrame(file_contents, columns=columns)
         data = data.astype(float)
         data['class'] = data['class'].astype(int)
-        # arbitrary_start_time = pd.Timestamp('2024-01-01 00:00:00')
-        # data['timestamp'] = pd.date_range(start=arbitrary_start_time, periods=len(data),
-        #                                   freq='20ms')  # 50 Hz = 20 milliseconds
 
         data = data[data['class'] != 0]  # Remove the in-between recordings
-        transformed_data = transform_data(data)
-        print(len(transformed_data))
-        if i == 9 or i == 10:
-            test_data = pd.concat([test_data, transformed_data])
-        else:
-            training_data = pd.concat([training_data, transformed_data])
+        X, y = transform_data(data)
+        if i == 1: #First training sample
+            X_train = X
+            y_train = y
+        elif i == 9: #First test sample
+            X_test = X
+            y_test = y
+        elif i == 10: #Second test sample
+            X_test = np.concatenate((X_test, X), axis=0)
+            y_test = np.concatenate((y_test, y), axis=0)
+        else: #All other training samples
+            X_train = np.concatenate((X_train, X), axis=0)
+            y_train = np.concatenate((y_train, y), axis=0)
 
-
-dotimeseriesclassification(training_data, test_data)
-#When random time periods are sampled from all data, performance is very good
-#When certain people are separated and designated as test, it doesn't work as well
-#It doesn't generalize well to other people, how can we fix that?
+dotimeseriesclassification(X_train, X_test, y_train, y_test)
